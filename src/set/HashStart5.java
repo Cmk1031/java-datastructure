339ba097144c0f808ace9a6bfd7783dcde1a7930
Cmk1031 <maple1031@naver.com>
2025-02-05 17:46:48 +0900

    set study commit

---
+++ src/set/HashStart5.java
@@ -0,0 +1,24 @@
+/*
+package set;
+
+import java.util.LinkedList;
+
+public class HashStart5 {
+    static final int CAPACITY = 10;
+
+    public static void main(String[] args) {
+        LinkedList<Integer>[] buckets = new LinkedList[CAPACITY];
+        for(int i=0; i<CAPACITY; i++) {
+            buckets[i] = new LinkedList<>();
+            add(buckets, 1);
+            add(buckets, 2);
+            add(buckets, 5);
+            add(buckets, 8);
+            add(buckets, 14);
+            add(buckets, 99);
+            add(buckets, 9); //중복
+            System.out.println("buckets = "+ Arrays.toString(buckets));
+        }
+    }
+}
+*/
